8907089d5b85b5c8f11a5fcd0dfee1e620b6ec2c
phyxolog <yzhivaga@gmail.com>
2018-05-19 15:08:58 +0300

    Implemented messages list (API)

--- lib/ptx/messages/messages.ex
+++ lib/ptx/messages/messages.ex
@@ -12,25 +12,41 @@ defmodule Ptx.Messages do
   use Ptx.Messages.Context.Read
   use Ptx.Messages.Context.Link
 
+  ## Send message to frontend about needed refresh markers on Gmail.
+  defp send_refresh_markers(message) do
+    params = %{
+      subject: message.subject,
+      readed_by: get_message_recipient(message),
+      when: message.first_readed_at,
+      sent_on: message.inserted_at,
+      recipients: message.recipients
+    }
+
+    PtxWeb.Endpoint.broadcast("room:#{message.sender_id}", "refresh_markers", params)
+  end
+
   def trace(nil), do: {:error, :empty_id}
   def trace(id) do
     case get_message(id) do
       {:ok, message} ->
         create_read(%{recepient: get_message_recipient(message), message: message})
 
-        {:ok, _user} = Accounts.fetch_user(id: message.sender_id)
+        {:ok, user} = Accounts.fetch_user(id: message.sender_id)
 
         read_message(message, fn
-          {:first_time, _message} ->
-            ## TODO: Send broadcast to message.sender_id (socket)
-            ## TODO: Send email notify of read emails
-            ## If user.notification_settings.email_readed
-            nil
-          {:once_again, _message} ->
-            ## Only if pro or trial account
-            ## And if user.notification_settings.once_again_readed_email
-            ## TODO: Send email notify of read emails
-            nil
+          {:first_time, message} ->
+            send_refresh_markers(message)
+
+            if user.notification_settings.email_readed do
+              ## TODO: Send email notify of read emails
+            end
+          {:once_again, message} ->
+            send_refresh_markers(message)
+
+            if user.plan in ["trial", "pro"] &&
+              user.notification_settings.once_again_readed_email do
+              ## TODO: Send email notify of read emails
+            end
         end)
       _ -> {:error, :not_found}
     end
