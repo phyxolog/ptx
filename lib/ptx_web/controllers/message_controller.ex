3fc8e74ff927078a8e965e169ba6401d8f57be3b
phyxolog <yzhivaga@gmail.com>
2018-05-24 14:58:42 +0300

    Fix

--- lib/ptx_web/controllers/message_controller.ex
+++ lib/ptx_web/controllers/message_controller.ex
@@ -33,12 +33,15 @@ defmodule PtxWeb.MessageController do
     |> json(%{messages: []})
   end
 
-  def create(_conn, _params, nil), do: {:error, :not_auth}
   def create(conn, params, user) do
-    with {:ok, %Message{} = _message} <- Messages.create_message(params, user) do
-      conn
-      |> put_status(:created)
-      |> json(%{status: :created})
+    case get_user(user, params["token"]) do
+      nil -> {:error, :not_auth}
+      user ->
+        with {:ok, %Message{} = _message} <- Messages.create_message(params, user) do
+          conn
+          |> put_status(:created)
+          |> json(%{status: :created})
+        end
     end
   end
 end
