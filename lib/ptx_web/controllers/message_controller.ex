c798d6a72cb8b5366f74ae7a7c3639d33e2f2e17
phyxolog <yzhivaga@gmail.com>
2018-05-24 13:48:04 +0300

    Fix message controller

--- lib/ptx_web/controllers/message_controller.ex
+++ lib/ptx_web/controllers/message_controller.ex
@@ -2,18 +2,30 @@ defmodule PtxWeb.MessageController do
   use PtxWeb, :controller
   use Guardian.Phoenix.Controller
 
-  alias Ptx.Messages
+  alias Ptx.{Messages, Accounts}
   alias Ptx.Messages.Message
 
   action_fallback PtxWeb.FallbackController
 
+  def index(conn, %{"token" => token} = params, _user) do
+    index(conn, params, Accounts.get_user_by_token(token))
+  end
+
   def index(_conn, _params, nil), do: {:error, :not_auth}
-  def index(_conn, %{"thread_ids" => thread_ids}, user) do
+  def index(conn, %{"thread_ids" => thread_ids}, user) do
     thread_ids = Enum.take(thread_ids, 1000)
     messages = Messages.list_messages_by_thread_ids(thread_ids, user.id)
-    {:ok, %{messages: messages}}
+
+    conn
+    |> put_resp_header("Access-Control-Allow-Origin", "*")
+    |> json(%{messages: messages})
   end
-  def index(_conn, _params, _user), do: {:ok, %{messages: []}}
+
+  def index(conn, _params, _user) do
+    conn
+    |> put_resp_header("Access-Control-Allow-Origin", "*")
+    |> json(%{messages: []})
+  end
 
   def create(_conn, _params, nil), do: {:error, :not_auth}
   def create(conn, params, user) do
