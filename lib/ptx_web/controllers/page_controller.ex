3021d5572fcbe86680346636da29610ed1d594f4
phyxolog <yzhivaga@gmail.com>
2018-05-25 14:14:57 +0300

    Fix pricing page

--- lib/ptx_web/controllers/page_controller.ex
+++ lib/ptx_web/controllers/page_controller.ex
@@ -2,8 +2,10 @@ defmodule PtxWeb.PageController do
   use PtxWeb, :controller
   use Guardian.Phoenix.Controller
 
-  def pricing(conn, _params, _user) do
-    render conn, "pricing.html"
+  def pricing(conn, _params, user) do
+    conn
+    |> assign(:user, user)
+    |> render("pricing.html")
   end
 
   def getting_started(conn, _params, _user) do
